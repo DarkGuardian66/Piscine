6547ff2a3e5c547fe96dd0f88694ae6116644767
DarkGuardian66 <asobasangprecieux@gmail.com>
2024-11-05 00:03:23 +0100

    version two of G_S

--- Golang/04_greatest_smallest/greatest_smallest.go
+++ Golang/04_greatest_smallest/greatest_smallest.go
@@ -10,22 +10,24 @@ import (
 
 func main() {
 
-	fmt.Println("entrez une liste des nombres en séparant les nombres par les espaces:")
+	fmt.Println("enter numbers seprated by spaces :")
 
 	Scanner := bufio.NewScanner(os.Stdin)
 	Scanner.Scan()
+
 	input := Scanner.Text()
+
 	tokens := strings.Fields(input)
 
 	if len(tokens) == 0 {
-		fmt.Println("la liste est vide")
+		fmt.Println("no input")
 		return
 	}
 
 	firstNumber, err := strconv.Atoi(tokens[0])
 
 	if err != nil {
-		fmt.Println("erreur de conversion", err)
+		fmt.Println("error on conversion", err)
 		return
 	}
 
@@ -33,22 +35,22 @@ func main() {
 
 	for _, token := range tokens[1:] {
 		number, err := strconv.Atoi(token)
+
 		if err != nil {
-			fmt.Println("problème de conversion", err)
+			fmt.Println("error on conversion", err)
 			return
 		}
 
 		if number < min {
 			min = number
-
 		}
+
 		if number > max {
 			max = number
 		}
-
 	}
 
-	fmt.Println("the greastest is: ", max)
-	fmt.Println("the smallest is :", min)
+	fmt.Println("the greastest number of your list is :", max)
+	fmt.Println("the smallest number of your list is :", min)
 
 }
